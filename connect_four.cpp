1b39ddc483e37fc8d519711f7a6c6081ed427173
CezarOS <132696740+SagittariusAStarr@users.noreply.github.com>
2025-02-24 00:15:38 +0100

    Connect four v0.5
    Added markers, where four has been connected
    Added colors indicating "X" or "O"

--- connect_four.cpp
+++ connect_four.cpp
@@ -1,13 +1,17 @@
-/*I forgot to add comms, therefore good luck finding out what is what
+/*
+I forgot to add comms, therefore good luck finding out what is what
 or not 'couse it is accualy simple code and project I guess (ofc if I didn't fucked it up somewhere)
 but it's working, I guess (I should stop overuse "I guess")
 */
 
-/*EDIT: 1 To do:
+/*
+EDIT: 2 To do:
 (DONE)Bug #1 when someone put char insted of int when choosing column, 
 game crashes (obviusly, 'couse I'm dump and forgor about that), I suppose
-2) Add marker to show where four has been connected
+(DONE) Add marker to show where four has been connected
+(IDEA) Add settings with board customisation
 */
+
 
 #include <iostream>
 #include <string>
@@ -83,7 +87,9 @@ void DrowWhoStartsFirst(){
 }
 
 void PlayerChoice(){
-    cout<<"Its "<<(turn == 1 ? "X":"O")<<" turn, choose which column you want to put your coin"<<endl;
+    cout<<"Its ";
+    turn == 1 ? cout<<"\033[31m"<<"X"<<"\033[0m" : cout<<"\033[34m"<<"O"<<"\033[0m";
+    cout<<" turn, choose which column you want to put your coin"<<endl;
     if(!(cin>>column)){
         cin.clear();
         cin.ignore(INT_MAX, '\n'); 
@@ -109,7 +115,7 @@ bool PutCoinsInBoard(){
     for(int i = NumberOfRows - 1; i > -1; i--){
         if(Board[i][column - 1] == " "){
             row = i + 1;
-            Board[i][column - 1] = (turn == 1 ? "X":"O");
+            Board[i][column - 1] = (turn == 1 ? "\033[31mX\033[0m":"\033[34mO\033[0m");
             character = Board[i][column - 1];
             turn == 1 ? turn = 0: turn = 1;
             return false;
@@ -125,31 +131,78 @@ int CheckForWin(int r, int c){
     int x = r;
     int y = c;
     int stack = 0;
+    int StR, StC, mark = 0;
     for(int i = 0; i < NumberOfColumns; i++){
-        if(Board[r][i] == character) stack++;
-        else stack = 0;
-        if(stack == 4) return 1;
+        if(Board[r][i] == character){
+            stack++;
+            if(mark == 0){
+                StC = i;
+                mark = 1;
+            }
+        }
+        else{
+            stack = 0;
+            mark = 0;
+        }
+        if(stack == 4){
+            for(int j = StC; j < StC + 4; j++){
+                Board[r][j] = (turn == 1 ? "\033[34m-\033[0m":"\033[31m-\033[0m");
+            }
+            return 1;
+        }
     }
+    mark = 0;
     stack = 0;
     for(int i = NumberOfRows - 1; i >= 0; i--){
-        if(Board[i][c] == character) stack++;
-        else stack = 0;
-        if(stack == 4) return 1;
+        if(Board[i][c] == character){
+            stack++;
+            if(mark == 0){
+                StR = i;
+                mark = 1;
+            }
+        }
+        else{
+            stack = 0;
+            mark = 0;
+        }
+        if(stack == 4){
+            for(int j = StR; j > StR - 4; j--){
+                Board[j][c] = (turn == 1 ? "\033[34m|\033[0m":"\033[31m|\033[0m");
+            }
+            return 1;
+        }
     }
     stack = 0;
+    mark = 0;
     while(true){
         if(x == 0 || y == 0) break;
         x--;
         y--;
     }
     while(x != NumberOfRows && y != NumberOfColumns){
-        if(Board[x][y] == character) stack++;
-        else stack = 0;
-        if(stack == 4) return 1;
+        if(Board[x][y] == character){
+            stack++;
+            if(mark == 0){
+                StR = x;
+                StC = y;
+                mark = 1;
+            }
+        }
+        else{
+            stack = 0;
+            mark = 0;
+        }
+        if(stack == 4){
+            for(int i = 0; i < 4; i++){
+                Board[StR + i][StC + i] = (turn == 1 ? "\033[34m\\\033[0m":"\033[31m\\\033[0m");
+            }
+            return 1;
+        }
         x++;
         y++;
     }
     stack = 0;
+    mark = 0;
     x = r;
     y = c;
     while(true){
@@ -158,9 +211,24 @@ int CheckForWin(int r, int c){
         y--;
     }
     while(x != 0 && y != NumberOfColumns){
-        if(Board[x][y] == character) stack++;
-        else stack = 0;
-        if(stack == 4) return 1;
+        if(Board[x][y] == character){
+            stack++;
+            if(mark == 0){
+                StR = x;
+                StC = y;
+                mark = 1;
+            }
+        }
+        else{
+            stack = 0;
+            mark = 0;
+        }
+        if(stack == 4){
+            for(int i = 0; i < 4; i++){
+                Board[StR - i][StC + i] = (turn == 1 ? "\033[34m/\033[0m":"\033[31m/\033[0m");
+            }
+            return 1;
+        }
         x--;
         y++;
     }
@@ -169,7 +237,7 @@ int CheckForWin(int r, int c){
 
 void EndingMessage(){
     cout<<endl<<"-=-=-=-=-=-=-=-=-"<<endl;
-    cout<<(turn == 1 ? "O":"X")<<" has won"<<endl;
+    cout<<(turn == 1 ? "\033[34mO\033[0m":"\033[31mX\033[0m")<<" has won"<<endl;
     cout<<"-=-=-=-=-=-=-=-=-"<<endl;
     return;
 }
